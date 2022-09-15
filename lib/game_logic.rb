7748c0d0cf8be80551c98a6ffd4c2fdd827229fd
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-15 09:06:04 -0500

    all green for winner calculations

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,7 +5,7 @@ module GameLogic
     winner = 0
 
     (0..5).each do |row|
-      winner = win?(@pieces[row])
+      winner = win_in_line?(@pieces[row])
       return winner unless winner.zero?
     end
 
@@ -13,7 +13,7 @@ module GameLogic
       column = [@pieces[0][column_number], @pieces[1][column_number],
                 @pieces[2][column_number], @pieces[3][column_number],
                 @pieces[4][column_number], @pieces[5][column_number]]
-      winner = win?(column)
+      winner = win_in_line?(column)
       return winner unless winner.zero?
     end
 
@@ -24,34 +24,55 @@ module GameLogic
       when 0
         left_diagonal = [@pieces[2][0], @pieces[3][1],
                          @pieces[4][2], @pieces[5][3]]
+        right_diagonal = [@pieces[0][3], @pieces[1][2],
+                          @pieces[2][1], @pieces[3][0]]
       when 1
         left_diagonal = [@pieces[1][0], @pieces[2][1],
                          @pieces[3][2], @pieces[4][3],
                          @pieces[5][4]]
+        right_diagonal = [@pieces[0][4], @pieces[1][3],
+                          @pieces[2][2], @pieces[3][1],
+                          @pieces[4][0]]
       when 2
         left_diagonal = [@pieces[0][0], @pieces[1][1],
                          @pieces[2][2], @pieces[3][3],
                          @pieces[4][4], @pieces[5][5]]
+        right_diagonal = [@pieces[0][5], @pieces[1][4],
+                          @pieces[2][3], @pieces[3][2],
+                          @pieces[4][1], @pieces[5][0]]
       when 3
         left_diagonal = [@pieces[0][1], @pieces[1][2],
                          @pieces[2][3], @pieces[3][4],
                          @pieces[4][5], @pieces[5][6]]
+        right_diagonal = [@pieces[0][6], @pieces[1][5],
+                          @pieces[2][4], @pieces[3][3],
+                          @pieces[4][2], @pieces[5][1]]
       when 4
         left_diagonal = [@pieces[0][2], @pieces[1][3],
                          @pieces[2][4], @pieces[3][5],
                          @pieces[4][6]]
+        right_diagonal = [@pieces[1][6], @pieces[2][5],
+                          @pieces[3][4], @pieces[4][3],
+                          @pieces[5][0]]
       when 5
         left_diagonal = [@pieces[0][3], @pieces[1][4],
                          @pieces[2][5], @pieces[3][6]]
+        right_diagonal = [@pieces[2][6], @pieces[3][5],
+                          @pieces[4][4], @pieces[3][5]]
       end
 
-      winner = win?(left_diagonal)
+      winner = win_in_line?(left_diagonal)
+      return winner unless winner.zero?
+
+      winner = win_in_line?(right_diagonal)
       return winner unless winner.zero?
     end
     0
   end
 
-  def win?(line, count1 = 0, count2 = 0)
+  private
+
+  def win_in_line?(line, count1 = 0, count2 = 0)
     line.each do |el|
       return 1 if count1 >= 4
       return -1 if count2 >= 4
--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -58,7 +58,7 @@ describe GameLogic do
     end
 
     context 'when player1 has won in a diagonal' do
-      it 'returns 1' do
+      it 'returns 1 for first downward diagonal option' do
         pieces = [[0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0],
                   [1, 0, 0, 0, 0, 0, 0],
@@ -68,23 +68,27 @@ describe GameLogic do
         game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
         expect(game.winner).to eq(1)
       end
-    end
-
-    context 'when player2 has won in a different diagonal' do
-      it 'returns -1' do
+      it 'returns 1 for a second downward diagonal option' do
         pieces = [[0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, -1, 0, 0, 0],
-                  [0, 0, 0, 0, -1, 0, 0],
-                  [0, 0, 0, 0, 0, -1, 0],
-                  [0, 0, 0, 0, 0, 0, -1]]
+                  [0, 1, 0, 0, 0, 0, 0],
+                  [0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 1, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0]]
         game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
-        expect(game.winner).to eq(-1)
+        expect(game.winner).to eq(1)
       end
-    end
-
-    context 'when player1 has won in an upward diagonal' do
-      it 'returns 1' do
+      it 'returns 1 for an upward diagonal option' do
+        pieces = [[0, 0, 0, 0, 0, 0, 1],
+                  [0, 0, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 1, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0]]
+        game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
+        expect(game.winner).to eq(1)
+      end
+      it 'returns 1 for a second upward diagonal' do
         pieces = [[0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 1, 0, 0],
                   [0, 0, 0, 1, 0, 0, 0],
@@ -96,7 +100,50 @@ describe GameLogic do
       end
     end
 
-    context 'when player2 has won and player1 has also played' do
+    context 'when player2 has won in a diagonal' do
+      it 'returns -1 for first downward diagonal option ' do
+        pieces = [[0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, -1, 0, 0, 0],
+                  [0, 0, 0, 0, -1, 0, 0],
+                  [0, 0, 0, 0, 0, -1, 0],
+                  [0, 0, 0, 0, 0, 0, -1]]
+        game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
+        expect(game.winner).to eq(-1)
+      end
+      it 'returns -1 for a second downward diagonal option' do
+        pieces = [[0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, -1, 0, 0, 0, 0],
+                  [0, 0, 0, -1, 0, 0, 0],
+                  [0, 0, 0, 0, -1, 0, 0],
+                  [0, 0, 0, 0, 0, -1, 0],
+                  [0, 0, 0, 0, 0, 0, 0]]
+        game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
+        expect(game.winner).to eq(-1)
+      end
+      it 'returns -1 for an upward diagonal option' do
+        pieces = [[0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, -1, 0, 0],
+                  [0, 0, 0, -1, 0, 0, 0],
+                  [0, 0, -1, 0, 0, 0, 0],
+                  [0, -1, 0, 0, 0, 0, 0]]
+        game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
+        expect(game.winner).to eq(-1)
+      end
+      it 'returns -1 for a second upward diagonal option' do
+        pieces = [[0, 0, 0, -1, 0, 0, 0],
+                  [0, 0, -1, 0, 0, 0, 0],
+                  [0, -1, 0, 0, 0, 0, 0],
+                  [-1, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0]]
+        game = ConnectFour.new('dummy display', %w[player1 player2], pieces)
+        expect(game.winner).to eq(-1)
+      end
+    end
+
+    context 'when player2 has won but player1 has also played' do
       it 'returns -1' do
         pieces = [[0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0],
